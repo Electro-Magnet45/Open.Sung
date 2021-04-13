9317ad1c206613292aa3b79eb2074412707f4826
TheAswin <75930721+TheAswin@users.noreply.github.com>
2021-04-13 12:07:58 +0530

    Third Commit

--- src/components/BottomPlayer.js
+++ src/components/BottomPlayer.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./BottomPlayer.css";
 
 import bottomPlayer__icon from "../images/BottomPlayer__icon.jpg";
+
+import { Slider } from "@material-ui/core";
 
 import {
   PlayCircle,
   ArrowRepeatAll,
 } from "@styled-icons/fluentui-system-filled";
+import {
+  SpeakerMute,
+  Speaker1,
+  Speaker2,
+} from "@styled-icons/fluentui-system-regular";
 import {
   SkipPrevious,
   SkipNext,
@@ -14,6 +21,12 @@ import {
 } from "@styled-icons/boxicons-regular";
 
 const BottomPlayer = () => {
+  const [volume, setVolume] = useState(0);
+
+  useEffect(() => {
+    console.log(volume);
+  }, [volume]);
+
   return (
     <div className="bottomPlayer">
       <div className="bottomPlayer__container">
@@ -36,10 +49,30 @@ const BottomPlayer = () => {
               <SkipNext className="bottomPlayer-cont-1-2-1_skip" />
               <ArrowRepeatAll className="bottomPlayer-cont-1-2-1_rest" />
             </div>
-            <div className="bottomPlayer-cont-1-2_controlSec"></div>
+
+            <div className="bottomPlayer-cont-1-2_controlSec">
+              <Slider className="bottomPlayer-cont-1_slider" />
+            </div>
           </div>
 
-          <div className="bottomPlayer-cont-1_toolsSec bottomPlayer-cont-1_sections"></div>
+          <div className="bottomPlayer-cont-1_toolsSec bottomPlayer-cont-1_sections">
+            <div className="bottomPlayer-cont-1-3_slider">
+              {volume < 1 && (
+                <SpeakerMute className="bottomPlayer-cont-1-3-1__icon" />
+              )}
+              {volume > 1 && volume < 50 && (
+                <Speaker1 className="bottomPlayer-cont-1-3-1__icon" />
+              )}
+              {volume > 49 && (
+                <Speaker2 className="bottomPlayer-cont-1-3-1__icon" />
+              )}
+              <Slider
+                className="bottomPlayer-cont-1_slider bottomPlayer-cont-1-3-1__slider"
+                value={volume}
+                onChange={(event, newVolume) => setVolume(newVolume)}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
