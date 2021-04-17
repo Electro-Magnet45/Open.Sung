0a99e7282119145a24bd7079d23c20d971cf62e7
TheAswin <75930721+TheAswin@users.noreply.github.com>
2021-04-17 19:01:31 +0530

    Initial nth Commit

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./TopHeader.css";
 
 import { IosArrowLeft } from "@styled-icons/fluentui-system-filled";
+import { Person } from "@styled-icons/fluentui-system-regular";
 
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -9,7 +10,18 @@ import { useHistory } from "react-router-dom";
 const TopHeader = () => {
   const history = useHistory();
 
+  const [openAccountBar, setOpenAccountBar] = useState(false);
   const historyPaths = useSelector((state) => state.history.paths);
+
+  useEffect(() => {
+    if (openAccountBar) {
+      window.addEventListener("click", (e) => {
+        if (!e.target.className.includes("topHeader-cont-2-1-over_visible")) {
+          setOpenAccountBar(false);
+        }
+      });
+    }
+  }, [openAccountBar]);
 
   return (
     <div className="topHeader">
@@ -30,7 +42,26 @@ const TopHeader = () => {
             onClick={() => history.goForward()}
           />
         </div>
-        <div className="topHeader-cont_searchSec"></div>
+        <div className="topHeader-cont_accountSec">
+          <div className="topHeader-cont-2_container">
+            <div
+              className="topHeader-cont-2-1_mainDiv"
+              onClick={() => setOpenAccountBar(true)}
+            >
+              <Person className="topHeader-cont-2-1_icon" />
+              <p>{"User Name"}</p>
+            </div>
+
+            <div
+              className={
+                "topHeader-cont-2-1_overlayDiv " +
+                (openAccountBar ? "topHeader-cont-2-1-over_visible" : "")
+              }
+            >
+              hello
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
